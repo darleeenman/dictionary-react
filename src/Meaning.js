16b6a173ede9b504491af368690fa89ec1331427
darleeenman <darleen@kindxd.com>
2023-02-08 09:56:24 -0800

    Update Meaning.js
    edit part of speech size to small

--- src/Meaning.js
+++ src/Meaning.js
@@ -4,12 +4,11 @@ export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <h3>{props.meaning.partOfSpeech}</h3>
-      {props.meaning.definitions.map(function(definition, index) {
+      <small>{props.meaning.partOfSpeech}</small>
+      {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
             <p>
-              {" "}
               {definition.definition}
               <br />
               <em>{definition.example}</em>
