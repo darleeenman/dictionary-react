1c30d992ce14cbd770798f27fcd09b1c429037ac
darleeenman <darleen@kindxd.com>
2023-02-17 16:28:20 -0800

    Update Dictionary.js
    update css file names
    create photos function + pexels api key/url

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./Dictionary.css";
+import "./dictionary.css";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
     setResults(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search(event) {
@@ -18,6 +24,14 @@ export default function Dictionary() {
     let apiURL = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
 
     axios.get(apiURL).then(handleResponse);
+
+    let pexelsApiKey =
+      "P4aBuFZdozP00POQFXn0Wx21uPEwythWIwjVE0HhcEhpS4xIhXgy3doc";
+
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleKeywordChange(event) {
@@ -28,13 +42,15 @@ export default function Dictionary() {
       <form onSubmit={search}>
         <input
           type="search"
-          placeholder="look for a word.." className="search"
+          placeholder="look for a word.."
+          className="search"
           onChange={handleKeywordChange}
         />
         <input type="submit" value="search" className="btn" />
       </form>
       <br />
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
