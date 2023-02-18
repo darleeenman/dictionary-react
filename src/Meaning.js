383c08e2ff30bd8ead79bc674936ef440d7f4db7
darleeenman <darleen@kindxd.com>
2023-02-17 16:27:29 -0800

    Update Meaning.js
    update file name
    change part of speech size

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,12 +1,12 @@
 import React from "react";
-import Synonyms from "./synonyms";
+import Synonyms from "./Synonyms";
 import "./meanings.css";
 
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
-      <small>{props.meaning.partOfSpeech}</small>
+      <h3>{props.meaning.partOfSpeech}</h3>
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
@@ -15,7 +15,7 @@ export default function Meaning(props) {
                 definition: {definition.definition}
               </div>
               <br />
-              <div className="example">Example: {definition.example}</div>
+              <div className="example">Ex. {definition.example}</div>
               <br />
               <Synonyms synonyms={definition.synonyms} />
               <div className="synonym">Synonyms: {definition.synonyms}</div>
