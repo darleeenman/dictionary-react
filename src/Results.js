433c92617c415a88fe488ed40c19233b283dc4c2
darleeenman <darleen@kindxd.com>
2023-02-17 16:56:55 -0800

    Update Results.js
    move h2 into section

--- src/Results.js
+++ src/Results.js
@@ -8,8 +8,8 @@ export default function Results(props) {
   if (props.results) {
     return (
       <div className="Results">
-        <h2>{props.results.word}</h2>
         <section>
+          <h2>{props.results.word}</h2>
           {props.results.phonetics.map(function (phonetic, index) {
             return (
               <div key={index}>
